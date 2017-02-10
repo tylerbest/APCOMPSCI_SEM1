07490e22972e5cc33713bf27be7604b975376cd7
tylerbest <tylerbest831@gmail.com>
2017-02-10 07:57:18 -0800

    v1

---
+++ Extra/toys.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class toys
+{
+	public static void main(Strin[]args)
+	{
+		ArrayList<Toys> inventory = new ArrayList<Toys>();
+		//inventory.add
+		
+		
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter your number of Toys");
+		String count = kb.nextLine();
+		Scanner scan = new Scanner(count);
+		
+		while(scan.hasNext())
+		{
+			String x = scan.next();
+			for(count : inventory)
+			{
+				if(b.scan()equals(x) || b.getName().equals(x))
+				{
+					
+				}
+			}
+		}
+	}
+}
